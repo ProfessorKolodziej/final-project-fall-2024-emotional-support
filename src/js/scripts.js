820d6179332031ece0ec8439e354aa423803b3a5
tim2003majesty <tim2003@bu.edu>
2025-04-22 11:47:29 -0400

    1

--- src/js/scripts.js
+++
@@ -1,5 +0,0 @@
-<button class="mood-btn" onclick="showMessage('happy')">😊 Happy</button>
-		<button class="mood-btn" onclick="showMessage('sad')">😢 Sad</button>
-		<button class="mood-btn" onclick="showMessage('angry')">😡 Angry</button>
-		<button class="mood-btn" onclick="showMessage('anxious')">😟 Anxious</button>
-		<button class="mood-btn" onclick="showMessage('fear')">😨 Fear</button>
